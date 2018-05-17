4faefd0397e9b0d8b500cd1142bd463df0c72f66
atmiyashita <atmiyashita@gmail.com>
2018-05-17 10:59:41 -0300

    Vitellogenin in-depth

--- vitellogenin-GtexDb.py
+++ vitellogenin-GtexDb.py
@@ -29,21 +29,54 @@ GeneFinder_tblastx(ProteinName="vitellogenin", db = "SRR6761207_TrinityAssembled
                     query_hits_i_hsps_Hsp =query_hits_i[5][0]
                     Eval = query_hits_i_hsps_Hsp[3].text #e-value
                     queryname = query[itr][2].text
-                    
+                    hitid = query_hits_i[1].text
                     if "vitellogenin" in queryname:
-                        if "vitellogenin receptor" not in queryname:
+                        if (("vitellogenin receptor" not in queryname) and
+                            ("vitellogenin convertase" not in queryname)):
                             if float(Eval) < 1e-20:
-                                res.write("Hit_id (fasta name in db) = " + query_hits_i[1].text +'\n') # print name of gene (fasta name) in database they the query sequence was matched to
-                                res.write("Hit_def (fasta name in db) = " + query_hits_i[2].text +'\n') # print name of gene (fasta name) in database they the query sequence was matched to 
-                                res.write("itr with hit = " + str(itr) +'\n' ) # print query number
-                                res.write("Query name = " + query[itr][2].text +'\n')   # print name of the query (gene name)
-                                res.write("Query length = " + query[itr][3].text +'\n') # print query length
-                                res.write("E-value = " + Eval +'\n') #print E-value
-                                res.write("=============================" +'\n')
+                                if (("47866" not in hitid) and # found to be vitellogenin-like
+                                ("50905" not in hitid) and # found to be vitellogenin-like
+                                ("41581" not in hitid) and # found to be vitellogenin-like
+                                ("45991" not in hitid) and # found to be vitellogenin-like
+                                ("22851" not in hitid) and # found to be vitellogenin-like
+                                ("30949" not in hitid) and # found to be vitellogenin-like
+                                ("43968" not in hitid) and # found to be vitellogenin-like
+                                ("51461" not in hitid) and # found to be vitellogenin-like
+                                ("43516" not in hitid) and # found to be vitellogenin-like
+                                ("48235" not in hitid) and # found to be vitellogenin-like
+                                ("44245" not in hitid) and # found to be APOLIPOPHORIN-like
+                                ("44675" not in hitid) and # found to be APOLIPOPHORIN-like
+                                ("31111" not in hitid) and # found to be APOLIPOPHORIN-like
+                                ("48628" not in hitid) and # found to be APOLIPOPHORIN-like
+                                ("51810" not in hitid) and # found to be vitellogenin receptor-like
+                                ("42335" not in hitid) and # found to be lipiphorin receptor-like
+                                ("51746" not in hitid) and # found to be lipophorin receptor-like
+                                ("51776" not in hitid) and # found to be lipophorin receptor-like
+                                ("51645" not in hitid) and # found to be lipophorin receptor-like
+                                ("32654" not in hitid) and # found to be lipophorin receptor-like
+                                ("30631" not in hitid) and # found to be lipiphorin receptor-like
+                                ("39999" not in hitid) and # found to be serin protease-like
+                                ("43254" not in hitid) and # found to be fibrilin-like
+                                ("27308" not in hitid) and # found to be lipophorin receptor-like
+                                ("47156" not in hitid) and # found to be fibrilin-like
+                                ("47865" not in hitid) and # found to be nidgen-like
+                                ("48867" not in hitid) and # found to be serin protease-like
+                                ("47223" not in hitid) and # found to be RING finger and CHY zinc finger domain-containing protein-like
+                                ("41675" not in hitid) and # found to be superoxide dismutase-like
+                                ("39212" not in hitid) and # found to be superoxide dismutase-like
+                                ("44053" not in hitid) and # found to be superoxide dismutase-like
+                                ("30657" not in hitid) and # found to be bromodomain-containing protein-like
+                                ("49021" not in hitid) and # found to be sortilin-related receptor-like
+                                ("49086" not in hitid)): # found to be vitellogenin-like
+                                    res.write("Hit_id (fasta name in db) = " + query_hits_i[1].text +'\n') # print name of gene (fasta name) in database they the query sequence was matched to
+                                    res.write("Hit_def (fasta name in db) = " + query_hits_i[2].text +'\n') # print name of gene (fasta name) in database they the query sequence was matched to 
+                                    res.write("itr with hit = " + str(itr) +'\n' ) # print query number
+                                    res.write("Query name = " + query[itr][2].text +'\n')   # print name of the query (gene name)
+                                    res.write("Query length = " + query[itr][3].text +'\n') # print query length
+                                    res.write("E-value = " + Eval +'\n') #print E-value
+                                    res.write("=============================" +'\n')
     res.close()
     
-    "abc" not in "abc dfegfsa"
-
 
 from GeneFinder import GeneFinder
 GeneFinder(ProteinName="vitellogenin", db = "SRR6761207_TrinityAssembled_180515.fasta")
